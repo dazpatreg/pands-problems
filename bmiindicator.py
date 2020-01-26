83f8cbb5b395c30004410dfa7ec76734c96e1c0e
unknown <dazpatreg@yahoo.ie>
2020-01-26 22:53:46 +0000

    Added new file for week 2 task calculating BMI

---
+++ bmiindicator.py
@@ -0,0 +1,10 @@
+#This is the piece of progamming to calculate the BMI
+
+weight = float(input("enter weight in kilograms")
+
+height = float(input("height in centimetres")
+
+
+bmi = weight / height 
+
+print("your BMI is", bmi)
